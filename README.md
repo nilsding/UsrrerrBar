44b19ed2d9005c3a1ed05cd0a6134cfaa6adbbc8
Georg Gadinger <nilsding@nilsding.org>
2022-05-13 16:08:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# UsrrerrBar
+
+A graphical user bars generator.
+
+## Requirements
+
+- Qt (tested with 6.2.1, but 5.12 should work too)
+- CMake (for building)
+
+## Building
+
+```sh
+# configure it
+cmake --preset dev
+
+# compile it
+cmake --build --preset dev
+```
---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+#include "config.h"
+
+int main(int argc, char** argv)
+{
+    QApplication a(argc, argv);
+
+    a.setApplicationName("UsrrerrBar");
+    a.setApplicationDisplayName("UsrrerrBar user bar generator");
+    a.setOrganizationName("rrerr.net");
+    a.setOrganizationDomain("rrerr.net");
+    a.setApplicationVersion(PROJECT_VERSION);
+
+    qSetMessagePattern(
+        "%{if-debug}D%{endif}%{if-info}I%{endif}%{if-warning}W%{endif}%{if-critical}C%{endif}%{if-fatal}F%{endif}, "
+        "[%{time yyyy-MM-ddTHH:mm:ss.zzz} #%{pid}] %{type} -- %{category}: %{message}"
+    );
+
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
